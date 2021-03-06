3b183d55d88eeb99fdb818ac40327ac7c216a983
Gabriel Hanssmann <74669444+GabrielHanssmann@users.noreply.github.com>
2021-03-06 16:05:02 -0300

    Add files via upload

--- Web Klick/assets/js/app.js
+++ Web Klick/assets/js/app.js
@@ -10,10 +10,29 @@ window.addEventListener('scroll', () => {
     header.classList.toggle('sticky', window.scrollY > 0);
   });
   
-  // FUNCION TOGGLE MENU HAMBURGER Y MENU TEXTO
+// FUNCION TOGGLE MENU HAMBURGER Y MENU TEXTO
   function toggleMenu(){
     var menuToggle = document.querySelector('.toggle');
     var menu = document.querySelector('.menu');
     menuToggle.classList.toggle('active');
     menu.classList.toggle('active');
   };
+  
+
+// FUNCION BACK TO TOP
+  jQuery("#backtotop").click(function(){
+    jQuery("body,html").animate({
+      scrollTop:0
+    },600);
+  });
+  
+  jQuery(window).scroll(function(){
+    if(jQuery(window).scrollTop() > 150)
+    {
+      jQuery("#backtotop").addClass("visible");
+    }
+    else
+    {
+      jQuery("#backtotop").removeClass("visible");
+    }
+  });
